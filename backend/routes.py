8d1683e1a50b87c859dd80f05c8ff5df39b5005f
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-09-24 15:00:52 +0400

    + .css
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,18 @@
 from backend import app
-from flask import render_template
+from flask import render_template,make_response
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Dmitry'}
-    return render_template('index.html',title='title',user=user)
+    R = render_template('index.html',title='jsF',user=user)
+    R = make_response(R)
+    R.headers['Content-Type'] = 'text/html; charset=utf-8'
+    return R
+
+@app.route(r'/black.css')
+def css():
+    R = render_template('black.css')
+    R = make_response(R)
+    R.headers['Content-Type'] = 'text/css; charset=utf-8'
+    return R
